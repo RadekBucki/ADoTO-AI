0f3b5531ae19aa006f167c17cc0bcca422221189
KamilPresler <kamilpfcb@gmail.com>
2023-04-03 21:38:41 +0200

    initial setup

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-if __name__ == '__main__':
-    print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "HelloWorld"
+
+
+@app.route('/test1')
+def hello():
+    return "Example output"
+
